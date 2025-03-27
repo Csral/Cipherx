fa582d02d4c5501361d8cfcd2b173e7edc4c34d7
God-Gamer-Manyu <rtamanyu@gmail.com>
2025-03-27 22:43:09 +0530

    fixed a package error

--- cipherx/src/main/java/com/godgamer/backend/Encryption.java
+++ cipherx/src/main/java/com/godgamer/backend/Encryption.java
@@ -3,5 +3,5 @@ package com.godgamer.backend;
 public class Encryption {
     // if while clicking run it opens UI, then either create test units or 
     // go to pom.xml and change the main class in the configuration to desired class
-    // e.g. <mainClass>com.godgamer.App</mainClass> --> <mainClass>com.godgamer.backend.Encryption</mainClass>
+    // e.g. <mainClass>com.godgamer.frontend.App</mainClass> --> <mainClass>com.godgamer.backend.Encryption</mainClass>
 }
