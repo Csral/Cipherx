aa2469f5c58030c3a7af5e02f8a90580a8282e30
God-Gamer-Manyu <rtamanyu@gmail.com>
2025-03-27 22:38:04 +0530

    Added way for backend to debug their code

--- cipherx/src/main/java/com/godgamer/backend/Encryption.java
+++ cipherx/src/main/java/com/godgamer/backend/Encryption.java
@@ -1,5 +1,7 @@
 package com.godgamer.backend;
 
 public class Encryption {
-
+    // if while clicking run it opens UI, then either create test units or 
+    // go to pom.xml and change the main class in the configuration to desired class
+    // e.g. <mainClass>com.godgamer.App</mainClass> --> <mainClass>com.godgamer.backend.Encryption</mainClass>
 }
