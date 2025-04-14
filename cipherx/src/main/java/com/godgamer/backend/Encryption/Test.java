47f0cbcff34c66d80c56ac3ffc8904b2cc4832fa
Chaturya <chaturyasral@gmail.com>
2025-04-14 09:27:39 +0530

    Use example test for aes

--- cipherx/src/main/java/com/godgamer/backend/Encryption/Test.java
+++ cipherx/src/main/java/com/godgamer/backend/Encryption/Test.java
@@ -1,52 +1,26 @@
 package com.godgamer.backend.Encryption;
 
 public class Test {
- 
+
     public static void main(String[] args) {
         
         try {
 
-            AES test = new AES();
-            test.MODIFY_IVSPACE();
-            test.MODIFY_KEY();
+            AES aes = new AES();
 
-            String m = test.encrypt_CTR("Hello!");
-            System.out.println("Data: Hello nig!\nEncrypted: "+ m);
+            String message = "This is a top secret message!";
 
-            String decoded = test.decrypt_CTR(m);
+            String encrypted = aes.encrypt_CTR(message, "temp"); // or encrypt_GCM, encrypt_CBC, etc.
+            System.out.println("Encrypted: " + encrypted);
 
-            System.out.println("Decoded: "+decoded);
+            String filename = aes.get_active_encrypted_filename(); // You must implement this method
 
-            String password = "pzSY$:C]&e";
-            
-            String svfile = test.KEY_SAVE_SECURE(true, "pas", 2, true);
+            AES aesDecryptor = new AES();
 
-            // String svfile = test.KEY_SAVE(true, "paedwfkghwr4uohfwsruikowhpoia");
+            String decrypted = aesDecryptor.decrypt_CTR(encrypted, filename, "temp"); // load key manually before this if needed
+            System.out.println("Decrypted: " + decrypted);
 
-//            String svfile = test.KEY_SAVE();
-
-            test.MODIFY_IVSPACE();
-            test.MODIFY_KEY();
-
-            decoded = test.decrypt_CTR(m);
-
-            System.out.println("Decoded: "+decoded);
-
-            // test.KEY_LOAD(svfile, true, "x3'6X{M05)1");
-
-            decoded = test.decrypt_CTR(m);
-
-            System.out.println("Decoded: "+decoded);
-
-            //test.KEY_LOAD(svfile, true, "paedwfkghwr4uohfwsruikowhpoia");
-            test.KEY_LOAD(svfile, true, "pas");
-            //test.KEY_LOAD(svfile);
-
-            decoded = test.decrypt_CTR(m);
-
-            System.out.println("Decoded: "+decoded);
-
-        } catch (Exception e) { 
+        } catch (Exception e) {
             System.out.println("Something went wrong: " + e);
         }
 
